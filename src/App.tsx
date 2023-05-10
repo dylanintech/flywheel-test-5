245b480bf8381d1b2ba12ac5d4fb304e5783ff5a
Dylan Molina <86082012+dylanintech@users.noreply.github.com>
2023-05-09 22:54:25 -0500

    changed to npm package hopefully

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Leaderboard } from "@dylanintech/flywheel-leaderboard";
+import { Leaderboard } from "flywheel-leaderboard";
 
 function App() {
 
